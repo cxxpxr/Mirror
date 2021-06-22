6ed841138eeab9ca4aae9a175f75bd0c2dbd1aa1
vis2k <info@noobtuts.com>
2021-06-22 21:19:01 +0800

    InsertIfNewEnough test

--- Assets/Mirror/Tests/Editor/NetworkTransform2k/SnapshotUtilsTests.cs
+++ Assets/Mirror/Tests/Editor/NetworkTransform2k/SnapshotUtilsTests.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -6,6 +6,47 @@ namespace Mirror.Tests.NetworkTransform2k
 {
     public class SnapshotUtilsTests
     {
+        [Test]
+        public void InsertIfNewEnough()
+        {
+            // empty buffer
+            SortedList<double, Snapshot> buffer = new SortedList<double, Snapshot>();
+
+            // inserting a first value should always work
+            Snapshot first = new Snapshot(1, Vector3.zero, Quaternion.identity, Vector3.one);
+            SnapshotUtils.InsertIfNewEnough(first, buffer);
+            Assert.That(buffer.Count, Is.EqualTo(1));
+
+            // insert before first should not work
+            Snapshot before = new Snapshot(0.5, Vector3.zero, Quaternion.identity, Vector3.one);
+            SnapshotUtils.InsertIfNewEnough(before, buffer);
+            Assert.That(buffer.Count, Is.EqualTo(1));
+
+            // insert after first should work
+            Snapshot second = new Snapshot(2, Vector3.left, Quaternion.identity, Vector3.right);
+            SnapshotUtils.InsertIfNewEnough(second, buffer);
+            Assert.That(buffer.Count, Is.EqualTo(2));
+            Assert.That(buffer.Values[0], Is.EqualTo(first));
+            Assert.That(buffer.Values[1], Is.EqualTo(second));
+
+            // insert between first and second should work (for now)
+            Snapshot between = new Snapshot(1.5, Vector3.zero, Quaternion.identity, Vector3.one);
+            SnapshotUtils.InsertIfNewEnough(between, buffer);
+            Assert.That(buffer.Count, Is.EqualTo(3));
+            Assert.That(buffer.Values[0], Is.EqualTo(first));
+            Assert.That(buffer.Values[1], Is.EqualTo(between));
+            Assert.That(buffer.Values[2], Is.EqualTo(second));
+
+            // insert after second should work
+            Snapshot after = new Snapshot(2.5, Vector3.zero, Quaternion.identity, Vector3.one);
+            SnapshotUtils.InsertIfNewEnough(after, buffer);
+            Assert.That(buffer.Count, Is.EqualTo(4));
+            Assert.That(buffer.Values[0], Is.EqualTo(first));
+            Assert.That(buffer.Values[1], Is.EqualTo(between));
+            Assert.That(buffer.Values[2], Is.EqualTo(second));
+            Assert.That(buffer.Values[3], Is.EqualTo(after));
+        }
+
         [Test]
         public void InterpolateSnapshot()
         {
