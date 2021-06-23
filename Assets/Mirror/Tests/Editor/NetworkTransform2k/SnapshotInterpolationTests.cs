ea610814cce37aaa54709757ca9ebbceb1c9d3c1
vis2k <info@noobtuts.com>
2021-06-23 18:30:16 +0800

    more tests

--- Assets/Mirror/Tests/Editor/NetworkTransform2k/SnapshotInterpolationTests.cs
+++ Assets/Mirror/Tests/Editor/NetworkTransform2k/SnapshotInterpolationTests.cs
@@ -347,5 +347,64 @@ namespace Mirror.Tests.NetworkTransform2k
             Assert.That(computed.transform.scale.y, Is.EqualTo(4.5).Within(Mathf.Epsilon));
             Assert.That(computed.transform.scale.z, Is.EqualTo(4.5).Within(Mathf.Epsilon));
         }
+
+        // fifth step: interpolation time overshoots the end
+        //             => test with additional snapshots now
+        [Test]
+        public void Compute_Step5_ExtrapolateWithMoreSnapshots()
+        {
+            // with remoteTime = 2.5 and delta of 0.5,
+            // compute sets remoteTime = 3.
+            // bufferTime = 2.
+            // so the threshold is bufferTime-remoteTime = 1.
+            // => everything has to be older than 1 (aka <= 1)
+            // => first at '0' is old enough
+            // => second at '1' is _exactly_ old enough via <=
+            Snapshot first = new Snapshot(0, new Vector3(1, 1, 1), Quaternion.Euler(new Vector3(0, 0, 0)), new Vector3(3, 3, 3));
+            Snapshot second = new Snapshot(1, new Vector3(2, 2, 2), Quaternion.Euler(new Vector3(0, 60, 0)), new Vector3(4, 4, 4));
+            // third snapshot should be a different direction so we can check
+            // if it really interpolates between second and third, and not just
+            // extrapolates between first and second.
+            // => simply double the value difference
+            Snapshot third = new Snapshot(2, new Vector3(4, 4, 4), Quaternion.Euler(new Vector3(0, 120, 0)), new Vector3(6, 6, 6));
+            buffer.Add(first.timestamp, first);
+            buffer.Add(second.timestamp, second);
+            buffer.Add(third.timestamp, third);
+
+            // compute with initialized remoteTime and buffer time of 2 seconds
+            // and a delta time to be sure that we move along it no matter what.
+            // -> interpolation time is already at '1' at the end.
+            // -> compute will add 0.5 deltaTime
+            // -> so we should overshoot aka extrapolate between first & second
+            float bufferTime = 2;
+            double deltaTime = 0.5;
+            double remoteTime = 2.5;
+            double interpolationTime = 1;
+            bool result = SnapshotInterpolation.Compute(bufferTime, deltaTime, ref remoteTime, ref interpolationTime, buffer, out Snapshot computed);
+
+            // should spit out the interpolated snapshot
+            Assert.That(result, Is.True);
+            // remote time should be moved along deltaTime
+            Assert.That(remoteTime, Is.EqualTo(2.5 + 0.5));
+            // interpolation started at the end = 1
+            // and deltaTime is 0.5, so we were at 1.5 internally.
+            // we have more snapshots, so we jump to the next and subtract '1'
+            // 1 + 0.5 = 1.5 => -1 => 0.5
+            Assert.That(interpolationTime, Is.EqualTo(0.5));
+            // buffer's first entry should have been removed
+            Assert.That(buffer.Count, Is.EqualTo(2));
+            // computed snapshot should be half way between second and third
+            Assert.That(computed.transform.position.x, Is.EqualTo(3).Within(Mathf.Epsilon));
+            Assert.That(computed.transform.position.y, Is.EqualTo(3).Within(Mathf.Epsilon));
+            Assert.That(computed.transform.position.z, Is.EqualTo(3).Within(Mathf.Epsilon));
+            // check rotation (epsilon is a bit too small)
+            Assert.That(computed.transform.rotation.eulerAngles.x, Is.EqualTo(0).Within(Mathf.Epsilon));
+            Assert.That(computed.transform.rotation.eulerAngles.y, Is.EqualTo(90).Within(0.001));
+            Assert.That(computed.transform.rotation.eulerAngles.z, Is.EqualTo(0).Within(Mathf.Epsilon));
+            // check scale
+            Assert.That(computed.transform.scale.x, Is.EqualTo(5).Within(Mathf.Epsilon));
+            Assert.That(computed.transform.scale.y, Is.EqualTo(5).Within(Mathf.Epsilon));
+            Assert.That(computed.transform.scale.z, Is.EqualTo(5).Within(Mathf.Epsilon));
+        }
     }
 }
